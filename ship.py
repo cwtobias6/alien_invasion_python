5d7649bfb4f16c76c5cef03306f2df0f85d6d591
Christian Tobias <cwtobias6@gmail.com>
2021-05-05 22:57:40 -0700

    Set Boundaries for screen

--- ship.py
+++ ship.py
@@ -4,12 +4,16 @@ class Ship():
 	"""docstring for Ship"""
 	def __init__(self, ai_game):
 		self.screen = ai_game.screen
+		self.settings = ai_game.settings
 		self.screen_rect = ai_game.screen.get_rect()
 
 		self.image = pygame.image.load("images/ship.bmp")
 		self.rect = self.image.get_rect()
 
 		self.rect.midbottom = self.screen_rect.midbottom
+
+		# Store a decimal value for the ship's horizontal position.
+		self.x = float(self.rect.x)
 
 		# Movement Flag
 		self.moving_right = False
@@ -18,10 +22,12 @@ class Ship():
 
 	def update(self):
 		""" Update ship's movement based on Movement Flag """
-		if self.moving_right:
-			self.rect.x += 1
-		elif self.moving_left:
-			self.rect.x -=1
+		if self.moving_right and self.rect.right < self.screen_rect.right:
+			self.x += self.settings.ship_speed
+		elif self.moving_left and self.rect.left > 0 :
+			self.x -= self.settings.ship_speed
+
+		self.rect.x = self.x
 
 	def blitme(self):
 		self.screen.blit(self.image,self.rect)
