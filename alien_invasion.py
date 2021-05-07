ef0ec5382f05bae59f850062df7e69195e9013e5
Christian Tobias <cwtobias6@gmail.com>
2021-05-06 22:08:29 -0700

    Fire bullets and refactor

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion():
 	"""docstring for AlienInvasion"""
@@ -14,13 +15,16 @@ class AlienInvasion():
 		pygame.display.set_caption("Alien Invasion")
 
 		self.ship = Ship(self)
+		self.bullets = pygame.sprite.Group()
 
 
 	def run_game(self):
 		while True:
 			self._check_events()
 			self._update_screen()
+			self._update_bullets()
 			self.ship.update()
+
 
 			# Make the most recently drawn screen visible.
 			pygame.display.flip()
@@ -44,6 +48,8 @@ class AlienInvasion():
 			self.ship.moving_left = True
 		elif event.key == pygame.K_q:
 			sys.exit()
+		elif event.key == pygame.K_SPACE:
+			self._fire_bullet()
 
 
 
@@ -53,10 +59,25 @@ class AlienInvasion():
 		elif event.key == pygame.K_LEFT:
 			self.ship.moving_left = False
 
+	def _fire_bullet(self):
+		if len(self.bullets) < self.settings.bullets_allowed:
+			new_bullet = Bullet(self)
+			self.bullets.add(new_bullet)
+
 	def _update_screen(self):
 		# Redraw the screen during each pass through the loop.
 		self.screen.fill(self.settings.bg_color)
 		self.ship.blitme()
+		for bullet in self.bullets.sprites():
+			bullet.draw_bullet()
+
+	def _update_bullets(self):
+		self.bullets.update()
+
+		for bullet in self.bullets.copy():
+			if bullet.rect.bottom <=0:
+				self.bullets.remove(bullet)
+
 
 
 
