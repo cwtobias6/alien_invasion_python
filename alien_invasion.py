8d2ff82da98987aa9c3b639c483d812366ad48ea
Christian Tobias <cwtobias6@gmail.com>
2021-05-09 16:28:06 -0700

    Refactor methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,9 +12,8 @@ class AlienInvasion():
 		pygame.init()
 		self.settings = Settings()
 
-		self.rounds = 0
 		self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
-		pygame.display.set_caption(f"Alien Invasion: {self.rounds}")
+		pygame.display.set_caption(f"Alien Invasion")
 
 		self.ship = Ship(self)
 
@@ -24,20 +23,41 @@ class AlienInvasion():
 		while True:
 			self._check_events()
 			self._update_screen()
+			self.ship.update()
+
+
 
 	def _check_events(self):
 		# Watch for keyboard and mouse events
 		for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					sys.exit()
+				elif event.type == pygame.KEYDOWN:
+					self._check_keydown_events(event)
+				elif event.type == pygame.KEYUP:
+					self._check_keyup_events(event)
+					
+	def _check_keydown_events(self, event):
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = True
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = True
+		elif event.key == K_q:
+			sys.exit()
+
+	def _check_keyup_events(self, event):
+		if event.key == pygame.K_RIGHT:
+			self.ship.moving_right = False
+		elif event.key == pygame.K_LEFT:
+			self.ship.moving_left = False
+
 
 	def _update_screen(self):
 		# Redraw the screen during each pass through the loop.
 			self.screen.fill(self.settings.bg_color)
 			self.ship.blitme()
-			
+
 		# Draw most current screen
-			self.rounds += 1
 			pygame.display.flip()
 
 if __name__ == "__main__":
--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship():
 	def __init__(self,ai_game):
 		""" Initialize ship and set starting position """
 		self.screen = ai_game.screen
+		self.settings = ai_game.settings
 		self.screen_rect = ai_game.screen.get_rect()
 
 		# Load ship image and get its rect
@@ -15,6 +16,27 @@ class Ship():
 		# Start ship and midbottom of the screen
 		self.rect.midbottom = self.screen_rect.midbottom
 
+		# Store a decimal value for the ship's horizontal position.
+		self.x = float(self.rect.x)
+
+		# Moving Flags
+		self.moving_right = False
+		self.moving_left = False
+
+
 	def blitme(self):
 		""" Draw ship at current location """
 		self.screen.blit(self.image, self.rect)
+
+	def update(self):
+		# Update the ship's x value, not the rect.
+		if self.moving_right and self.rect.right < self.screen_rect.right:
+			self.x += self.settings.ship_speed
+		elif self.moving_left and self.rect.left > 0:
+			self.x -= self.settings.ship_speed
+
+		# Update rect object from self.x.
+		self.rect.x = self.x
+
+
+
